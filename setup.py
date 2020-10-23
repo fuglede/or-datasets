24cbe48b780fbc40e48e609da46585537afe0a08
Simon Spoorendonk <simon@flowty.ai>
2020-10-23 15:50:56 +0200

    package

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+__version__ = None
+exec(open("or_datasets/_version.py").read())
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="or-datasets",
+    version=__version__,
+    author="Simon Spoorendonk",
+    author_email="info@flowty.ai",
+    url="https://flowty.ai",
+    description="OR Datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["or_datasets"],
+    python_requires=">=3.6",
+    classifiers=[
+        "License :: MIT",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords=[
+        "Optimization",
+        "Nework Optimization",
+        "Combinatorial Optimization",
+        "Linear Programming",
+        "Integer Programming",
+        "Operations Research",
+        "Mathematical Programming",
+    ],
+)
